2148630c6743389abf7a3acf8f48e74dc34ec38d
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-06-01 13:35:31 -0500

    feat(chapter-4): add starter and completed java files.

---
+++ starter-files/lesson-04/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("--- Montaña Rusa 'El Dragón' ---");
+        
+        // La caja mágica con el dato del visitante
+        // Altura en metros
+        
+        // Empezamos a tomar decisiones
+        
+        // Primera pregunta: Si es demasiado alto
+        if (alturaVisitante >= 2.00) {
+            
+        // Segunda pregunta: Si cumple la altura estándar
+        } else if (alturaVisitante >= 1.40) {
+            
+        // El camino que se toma si ninguna pregunta anterior recibe un "Verdadero"
+        } else {
+            
+        }
+        
+    }
+}
+```
