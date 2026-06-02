cc62c4633243885e1ee143b3760d5fe3b3fe726d
李 <miauperuvian@gmail.com>
2026-06-01 21:32:41 -0500

    feat(chapter-5): add starter and completed java files.

---
+++ starter-files/lesson-05/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    
+    public static void main(String[] args) {
+
+        System.out.println("--- 1. Bucle While: Entrenamiento de Energía ---");
+        // "Mientras (while) la energía sea mayor que 0..."
+
+
+
+            // CRÍTICO: Reducir la energía para evitar un Bucle Infinito
+            // Podemos usar energia = energia - 1; o el atajo energia--;
+
+
+
+        System.out.println("\n--- 2. Bucle For: Cuenta de Flexiones ---");
+        // El bucle for agrupa 3 partes en sus paréntesis: 
+        // 1. Contador inicial (int i = 1)
+        // 2. Condición para detenerse (i <= 5)
+        // 3. Cómo avanza (i++ que suma de uno en uno)
+        
+        
+    }
+}
