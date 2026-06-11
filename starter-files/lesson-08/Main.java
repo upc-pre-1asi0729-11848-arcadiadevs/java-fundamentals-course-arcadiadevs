258e57527e2d1ae26f7c567fb646f186b85b23cf
DaronCameloft <daroncameloftdev@gmail.com>
2026-06-10 22:40:42 -0500

    feat(chapter-8): add starter java files for lesson 8.

---
+++ starter-files/lesson-08/Main.java
@@ -0,0 +1,12 @@
+// LA CLASE PROTEGIDA (El Molde Seguro)
+class Heroe {
+// La palabra 'private' prohibe que alguien cambie esto desde otro archivo
+    private String
+}
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
